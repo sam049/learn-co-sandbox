b24fc11dca893c3ecbf323d9683cf2024f2561c2
Sam Miller <sammillerks@gmail.com>
2019-03-14 03:02:36 +0000

    Automatically backed up by Learn

---
+++ test.js
@@ -0,0 +1,13 @@
+var index = require("./index.js")
+
+if (index.name === "Susan") {
+   console.log("The name is correct!")
+} else {
+    console.log("The name is not correct, the correct name is" + index.name)
+}
+
+if (index.height === 74) {
+  console.log("The height is rizzy")
+} else {
+    console.log("The height is not correct, the correct height is " + index.height)
+}
